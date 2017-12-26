821585d8c667f6f6ddb56614b5eae0611a835255
Masood <masood_hasan@msn.com>
2017-12-26 21:29:01 +0500

    added React Project

--- webProjects/nodeTest/file.js
+++ webProjects/nodeTest/file.js
@@ -2,6 +2,5 @@ const file = require('fs');
 const path = require('path');
 var fileName = path.join(__dirname, 'testing.txt')
 var testFile = file.readFileSync(fileName, 'utf8')
-const newLocal = file.writeFileSync;
-newLocal('writeMe.txt', testFile)
-console.log(testFile)
+file.writeFileSync('writeMe.txt', testFile)
+Console.log(testFile)
