af9a68ea9ad6af61e0f445f7ce0b92765fbbfc8f
Masood <masood_hasan@msn.com>
2018-01-08 19:19:34 +0500

    changes made to nodeJS folder and Angular component

--- webProjects/nodeTest/server.js
+++ webProjects/nodeTest/server.js
@@ -1,1 +1,18 @@
-var http = require("http");
+//dependencies
+const http = require('http')
+//start of the server
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end("geeee")
+    console.log('requesting: ' + req.url)
+})
+server.listen(4500);
+//server initialization message after 3 seconds
+var serverCall = setInterval(function(){
+    let x = false
+    console.log("Server Started")
+    x = true
+    if(x == true){
+        clearTimeout(serverCall)
+    }
+}, 3000)
