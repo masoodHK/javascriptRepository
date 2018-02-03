27e0165bd2a22230cda752e55caa3be1acfb545f
Masood <masood_hasan@msn.com>
2018-02-03 11:46:14 +0500

    firebase deployed

--- C-C++Projects/test.c
+++ C-C++Projects/test.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-#include <conio.h>
-#include <math.h>
-#include <string.h>
-//Called Functions
-int someFunction(int n1, int n2);
-//Function Descriptions
-int someFunction(int n1, int n2){
-     return n1 + n2;
+#define MAX_NAME_SIZE 256
+void simple_welcome(void){
+    char buffer[32]; // to hold the player's name
+    printf("Please enter your name: ");
+    fgets(buffer, MAX_NAME_SIZE, stdin); // input from 'Standard In'
+    printf("Welcome ");
+    printf(buffer);
+    return;
 }
-//Main Program
-void main(void){
-     int n1, n2;
-     scanf("%d%d",&n1, &n2);
-     int sum = 0;
-     sum=someFunction(n1,n2);
-     printf("The sum of %d and %d is %d",n1, n2, sum);
+int main(void){
+    char some_space[80];
+    simple_welcome();
+    return 0;
 }
--- webProjects/nodeTest/angular/angular-app/src/app/app.module.ts
+++ webProjects/nodeTest/angular/angular-app/src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
