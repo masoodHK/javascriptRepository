866e2a25493929d8091e59ce1efe156c50d46250
Masood <masood_hasan@msn.com>
2018-02-15 03:22:58 +0500

    added changes to many files and deployed angular app to firebase

--- webProjects/nodeTest/angular/angular-app/src/app/app.module.ts
+++ webProjects/nodeTest/angular/angular-app/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- webProjects/nodeTest/stream.js
+++
@@ -1,9 +0,0 @@
-const stream = require('fs');
-const http = require('http');
-var ReadStream = stream.createReadStream(__dirname + '/testing.txt', 'utf-8');
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    ReadStream.pipe(res)
-    console.log('requesting: ' + req.url)
-}).listen(4500);
