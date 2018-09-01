255d139c1e0ff62057e0bf4464e3fc2d518e6e64
Masood <masood_hasan@msn.com>
2018-09-02 00:59:01 +0500

    complete the assignment # 3

---
+++ webProjects/reactTest/assignment-3/admin-panel/src/components/employeeTable/EmployeeTable.js
@@ -0,0 +1,285 @@
+import React, { Component } from 'react';
+import './EmployeeTable.css';
+import swal from 'sweetalert';
+import Employee from './Employee';
+
+export default class EmployeeTable extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            listOfEmployees: [
+                {
+                    id: 1,
+                    name: "Leanne Graham",
+                    email: "Sincere@april.biz",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 2,
+                    name: "Ervin Howell",
+                    email: "Shanna@melissa.tv",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 3,
+                    name: "Clementine Bauch",
+                    email: "Nathan@yesenia.net",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 4,
+                    name: "Patricia Lebsack",
+                    email: "Julianne.OConner@kory.org",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 5,
+                    name: "Chelsey Dietrich",
+                    email: "Lucio_Hettinger@annie.ca",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 6,
+                    name: "Mrs. Dennis Schulist",
+                    email: "Karley_Dach@jasper.info",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 7,
+                    name: "Kurtis Weissnat",
+                    email: "Telly.Hoeger@billy.biz",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 8,
+                    name: "Nicholas Runolfsdottir V",
+                    email: "Sherwood@rosamond.me",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 9,
+                    name: "Glenna Reichert",
+                    email: "Chaim_McDermott@dana.io",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                },
+                {
+                    id: 10,
+                    name: "Clementina DuBuque",
+                    email: "Rey.Padberg@karina.biz",
+                    salary: 0,
+                    joiningDate: 'mm-dd-yyyy'
+                }
+            ],
+            employeeFormStatus: false,
+            inputEmployeeID: null,
+            inputEmployeeName: "",
+            inputEmployeeSalary: 0,
+            inputEmployeeEmail: "",
+            inputEmployeeJoiningDate: "",
+            formMethod: ""
+        }
+
+        this.editEmployeeData = this.editEmployeeData.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
+        this.submit = this.submit.bind(this);
+        this.employeeForm = this.employeeForm.bind(this);
+        this.handleEmployeeNameChange = this.handleEmployeeNameChange.bind(this)
+        this.handleEmployeeSalaryChange = this.handleEmployeeSalaryChange.bind(this)
+        this.handleEmployeeEmailChange = this.handleEmployeeEmailChange.bind(this)
+        this.handleEmployeeJoiningDateChange = this.handleEmployeeJoiningDateChange.bind(this)
+    }
+
+    employeeForm() {
+        this.setState({
+            employeeFormStatus: true,
+            formMethod: "add"
+        });
+    }
+
+    handleEmployeeSalaryChange(event) {
+        this.setState({inputEmployeeSalary: event.target.value})
+    }
+
+    handleEmployeeEmailChange(event) {
+        this.setState({inputEmployeeEmail: event.target.value})
+    }
+
+    handleEmployeeJoiningDateChange(event) {
+        this.setState({inputEmployeeJoiningDate: event.target.value})
+    }
+
+    handleEmployeeNameChange(event) {
+        this.setState({inputEmployeeName: event.target.value})
+    }
+    
+    editEmployeeData(employeeID) {
+        const employee = this.state.listOfEmployees.filter(e => e.id === employeeID)
+        console.log(employee)
+        this.setState({
+            employeeFormStatus:true,
+            inputEmployeeName: employee[0].name,
+            inputEmployeeEmail: employee[0].email,
+            inputEmployeeSalary: employee[0].salary,
+            inputEmployeeJoiningDate: employee[0].joiningDate,
+            inputEmployeeID: employeeID,
+            formMethod: "edit"
+        });
+
+    }
+
+    deleteEmployee(employeeID) {
+        const { listOfEmployees } = this.state;
+        const updatedList = listOfEmployees.filter(employee => {
+            if(employee.id !== employeeID) {
+                return employee;
+            }
+            return null
+        });
+
+        this.setState({listOfEmployees: updatedList});
+        swal({
+            title: "Success",
+            text: "The emloyee has been deleted!",
+            icon: "success",
+        })
+    }
+    
+    submit(event) {
+        event.preventDefault();
+
+        const {
+            listOfEmployees,
+            formMethod,
+            inputEmployeeID,
+            inputEmployeeEmail,
+            inputEmployeeName,
+            inputEmployeeJoiningDate,
+            inputEmployeeSalary
+        } = this.state
+        switch(formMethod){
+            case "add":
+                const employeeData = {
+                    id: listOfEmployees.length + 1,
+                    name: inputEmployeeName,
+                    email: inputEmployeeEmail,
+                    salary: inputEmployeeSalary,
+                    joiningDate: inputEmployeeJoiningDate
+                }
+        
+                listOfEmployees.push(employeeData)
+                this.setState({employeeFormStatus:false, listOfEmployees});
+                swal({
+                    title: "Success",
+                    text: "The emloyee data has been added!",
+                    icon: "success",
+                })
+                break
+            case "edit":
+                const updatedList = listOfEmployees.map(employee => {
+                    if(employee.id === inputEmployeeID) {
+                        employee.name = inputEmployeeName
+                        employee.salary = inputEmployeeSalary
+                        employee.joiningDate = inputEmployeeJoiningDate
+                        employee.email = inputEmployeeEmail
+                    }
+                    return employee
+                });
+                this.setState({
+                    employeeFormStatus: false,
+                    listOfEmployees: updatedList,
+                    inputEmployeeID: null,
+                    inputEmployeeEmail: "",
+                    inputEmployeeName: "",
+                    inputEmployeeJoiningDate: "",
+                    inputEmployeeSalary: "",
+                })
+                swal({
+                    title: "Success",
+                    text: "The emloyee data has been edited!",
+                    icon: "success",
+                })
+                break
+            default: break
+        }
+        
+    }
+
+    render() {
+        const {
+            inputEmployeeEmail,
+            inputEmployeeSalary,
+            inputEmployeeName,
+            inputEmployeeJoiningDate } = this.state
+        if(this.state.employeeFormStatus === true) {
+            return (
+                <form onSubmit={this.submit}>
+                    <input
+                        type="text"
+                        placeholder="Employee Name"
+                        value={inputEmployeeName}
+                        onChange={this.handleEmployeeNameChange}/>
+                    <input
+                        type="email"
+                        placeholder="Employee Email"
+                        value={inputEmployeeEmail}
+                        onChange={this.handleEmployeeEmailChange}/>
+                    <input 
+                        type="number"
+                        placeholder="Employee Salary"
+                        value={inputEmployeeSalary}
+                        onChange={this.handleEmployeeSalaryChange}/>
+                    <input
+                        type="text"
+                        className="datepicker"
+                        value={inputEmployeeJoiningDate}
+                        onChange={this.handleEmployeeJoiningDateChange}/>
+                    <button className="btn" type="submit">Submit</button>
+                </form>
+            )
+        }
+        
+        else {
+
+            return (
+                <div>
+                    <button className="btn" onClick={this.employeeForm}>Add an Employee</button>
+                    <table className="striped">
+                        <thead>
+                            <tr>
+                                <th>Employee Name</th>
+                                <th>Email</th>
+                                <th>Salary</th>
+                                <th>Joining Date</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.listOfEmployees.map(employeeData => {
+                                return <Employee
+                                    key={employeeData.id}
+                                    name={employeeData.name}
+                                    email={employeeData.email}
+                                    salary={employeeData.salary}
+                                    joiningDate={employeeData.joiningDate}
+                                    edit={() => this.editEmployeeData(employeeData.id)}
+                                    delete={() => this.deleteEmployee(employeeData.id)}/>
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
+    }
+}
--- webProjects/reactTest/assignment-3/admin-panel/src/components/login/Login.js
+++ webProjects/reactTest/assignment-3/admin-panel/src/components/login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import swal from 'sweetalert';
+import EmployeeTable from '../employeeTable/EmployeeTable';
 import './Login.css'
 
 export default class Login extends Component {
@@ -7,17 +8,17 @@ export default class Login extends Component {
         super(props)
 
         this.state = {
-            username: "admin@email.com",
+            username: "admin@domain.com",
             password: "admin",
             isLoggedIn: false,
             inputUsername: "",
             inputPassword: ""
         }
-
-
+        
         this.authenticate = this.authenticate.bind(this)
         this.handleUsernameChange = this.handleUsernameChange.bind(this)
         this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.signOut = this.signOut.bind(this);
     }
 
     authenticate(event) {
@@ -30,6 +31,7 @@ export default class Login extends Component {
                 text: "You are logged in!",
                 icon: "success",
             });
+            this.setState({ isLoggedIn: true, inputUsername: "", inputPassword: "" })
         }
         else {
             swal({
@@ -48,30 +50,52 @@ export default class Login extends Component {
         this.setState({inputPassword: event.target.value})
     }
 
-    render() {
-        if(this.isLoggedIn) {
-            return
-        }
-        return (
-            <form onSubmit={this.authenticate} className="form">
-                <h2>Login</h2>
-                <div className="input-field">
-                    <input
-                        type="email"
-                        value={this.state.inputUsername}
-                        onChange={this.handleUsernameChange} 
-                        placeholder="Email: admin@email.com" />
-                </div>
-                <div className="input-field">
-                    <input
-                        type="password"
-                        value={this.state.inputPassword}
-                        onChange={this.handlePasswordChange} 
-                        placeholder="Password: admin"/>
-                </div>
-                <button type="submit" className="btn">Submit</button>
-            </form>
-        );
+    signOut() {
+        this.setState({isLoggedIn: false});
+    }
 
+    render() {
+        if(this.state.isLoggedIn === true) {
+            return (
+                <div>
+                    <header className="App-header">
+                        <h1 className="App-title">Admin Panel</h1>
+                        <button className="btn" onClick={this.signOut}>Sign Out</button>
+                    </header>
+                    <div className="container">
+                        <EmployeeTable />
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <header className="App-header">
+                        <h1 className="App-title">Admin Panel</h1>
+                    </header>
+                    <div className="container">
+                        <form onSubmit={this.authenticate} className="form">
+                            <h2>Login</h2>
+                            <div className="input-field">
+                                <input
+                                    type="email"
+                                    value={this.state.inputUsername}
+                                    onChange={this.handleUsernameChange} 
+                                    placeholder="Email: admin@domain.com" />
+                            </div>
+                            <div className="input-field">
+                                <input
+                                    type="password"
+                                    value={this.state.inputPassword}
+                                    onChange={this.handlePasswordChange} 
+                                    placeholder="Password: admin"/>
+                            </div>
+                            <button type="submit" className="btn">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            );
+        }
     }
 }
