fb68a7acc9440ce4379f0aac5fae08a617f78c3a
Masood <masood_hasan@msn.com>
2018-10-07 02:18:52 +0500

    complete the Post App

--- webProjects/reactTest/react-post-display/src/App.js
+++ webProjects/reactTest/react-post-display/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import img from "./assets/Firefox_wallpaper.png";
+import post1 from "./assets/post1.jpeg";
+import post2 from "./assets/post2.jpg";
+import post3 from "./assets/post3.jpeg";
+import avatar from "./assets/index.jpeg";
+import Post from './Component/Post';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    let imgArr = [img, post1, post2, post3];
+    const postObj = {
+      created_by: "Masood Hasan Khan",
+      avatar: avatar,
+      imgArr,
+      descirption: "Image Gallery",
+      date: Date.now(),
+      likes: ['a','b','c','d','e'],
+    };
+    
     return (
       <div className="App">
-      
+        <Post data={postObj} />
       </div>
     );
   }
--- webProjects/reactTest/react-post-display/src/Component/Post.js
+++ webProjects/reactTest/react-post-display/src/Component/Post.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
 import FbImageLibrary from 'react-fb-image-grid';
 import FacebookEmoji from "react-facebook-emoji";
+import moment from 'moment';
+
+function HoverReaction(props) {
+    if(props.hovered) {
+        return (
+            <div className="display">
+                <FacebookEmoji type="like"/>
+                <FacebookEmoji type="love"/>
+                <FacebookEmoji type="wow"/>
+                <FacebookEmoji type="yay"/>
+                <FacebookEmoji type="angry"/>
+                <FacebookEmoji type="haha"/>
+                <FacebookEmoji type="sad"/>
+            </div>
+        )
+    }
+    else {
+        return null
+    }
+}
 
 export default class Post extends Component {
-    render() {
+    state = {
+        comment: "",
+        hovered: false
+    }
 
+    handleComment = (event) => {
+        this.setState({ comment: event.target.value })
+    }
+    render() {
+        const { data } = this.props
         return (
-            <div>
-                <FbImageLibrary images={[]}/>
+            <div className="post" key="te">
+                <div className="user-info container">
+                    <img src={data.avatar} alt="Profile"/>
+                    <div>
+                        <p>{data.created_by}</p>
+                        <p>{moment(data.date).fromNow(true)} ago</p>
+                    </div>
+                </div>
+                <FbImageLibrary images={data.imgArr} />
+                <div className="feedback">
+                    <div className="container">
+                        <HoverReaction hovered={this.state.hovered}/>
+                        <div className="reaction">
+                            <p
+                            onMouseEnter={() => this.setState({hovered: true})}
+                            onMouseDown={() => this.setState({hovered: false})}>Like</p>
+                            <p>Comment</p>
+                            <p>Share</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
